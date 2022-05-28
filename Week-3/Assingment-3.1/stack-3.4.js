9c7c66cb9fd0e711ac2fb2c39e65de8e2344baa4
Aditya Chaudhari <adi.chaudhari1997@gmail.com>
2022-05-28 10:56:06 +0530

    Added explanation to stack problem

--- Week-3/Assingment-3.1/stack-3.4.js
+++ Week-3/Assingment-3.1/stack-3.4.js
@@ -1,16 +1,54 @@
 function createStack(){
     const items=[];
-    function push(item){
+    return {
+        push(item){
             items.push(item);
-    }
-    function pop(){
+        },
+        pop(){
             return items.pop();
-    }
+        }
+    };
 }
 
 const stack = createStack();
 stack.push(10);
 stack.push(11);
-console.log(stack.pop());
-// stack.items=[12,13];
+stack.pop();
+// stack.items.push(13);
 console.log(stack.items);
+
+// Output:
+// Here line 17 generate error as we can't access items outside the function
+// If we comment then also we get : "undefined"
+// as we can't access items outside function
+
+
+// --------------------------------------------------------------------
+// Explanation:
+// --------------------------------------------------------------------
+// The output of above code : "undefined"
+// beacuse we declare items array inside the function scope
+// but in the below code items is declared at the time of return
+
+
+
+// function createStack(){
+//     return {
+//         items:[],
+//         push(item){
+//             this.items.push(item);
+//         },
+//         pop(){
+//             return this.items.pop();
+//         }
+//     };
+// }
+// const stack = createStack();
+// stack.push(10);
+// stack.push(11);
+// stack.pop();
+// stack.items.push(13)
+// console.log(stack.items);
+
+// Ouput:
+// [10,13]
