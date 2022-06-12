fe93a65c008ce78a3ff81a89ffb32f3791fe6101
Aditya Chaudhari <adi.chaudhari1997@gmail.com>
2022-06-12 19:52:08 +0530

    added negative test case in assignment-5.4 & ignored package.json

--- Week-5/Assignment-5.4/Assignment-5.4.test.js
+++ Week-5/Assignment-5.4/Assignment-5.4.test.js
@@ -9,6 +9,9 @@ describe("Math Operations Test: ", ()=>{
     }),
     test("Test for product of 3 * 2 should return 6 ",()=>{
         expect(mathOperations.product(3,2)).toBe(6);
+    }),
+    test("Neagtive Test for add of string '3' + '2' should return '23' which is not correct ",()=>{
+        expect(mathOperations.sum('3','2')).toBe("32");
     });
 });
 
@@ -20,17 +23,18 @@ describe("Math Operations Test: ", ()=>{
 // PS F:\pesto-submissions\p2-anil-aditya226369\Week-5> cd .\Assignment-5.4\
 // PS F:\pesto-submissions\p2-anil-aditya226369\Week-5\Assignment-5.4> npm test
 
-// > test
+// > week-5@1.0.0 test
 // > jest
 
 //  PASS  Assignment-5.4/Assignment-5.4.test.js
 //   Math Operations Test:
-//     √ Test for addition of 1 + 2 should return 3  (3 ms)
+//     √ Test for addition of 1 + 2 should return 3  (4 ms)
 //     √ Test for difference of 3 - 2 should return 1  (1 ms)
 //     √ Test for product of 3 * 2 should return 6  (1 ms)
+//     √ Neagtive Test for add of string '3' + '2' should return '23' which is not correct  (1 ms)
 
 // Test Suites: 1 passed, 1 total
-// Tests:       3 passed, 3 total
+// Tests:       4 passed, 4 total
 // Snapshots:   0 total
-// Time:        0.659 s, estimated 1 s
+// Time:        1.104 s
 // Ran all test suites.
